672d319ffc2af065ab6d3b86de88c8b2e63550e2
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2025-03-15 21:20:38 +0100

    Fix format

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,8 +2,8 @@ import { drizzle as drizzleSqlite } from "drizzle-orm/libsql";
 import { drizzle as drizzlePostgres } from "drizzle-orm/node-postgres";
 
 export const cmsDb = drizzlePostgres(process.env.CMS_DATABASE_URL!, {
-    logger: process.env.NODE_ENV === 'development',
+  logger: process.env.NODE_ENV === "development",
 });
 export const terryDb = drizzleSqlite(process.env.TERRY_DATABASE_URL!, {
-    logger: process.env.NODE_ENV === 'development',
+  logger: process.env.NODE_ENV === "development",
 });
