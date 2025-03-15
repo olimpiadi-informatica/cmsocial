be84b2e5d5422333495a6a246fccf1108f709aa5
William Di Luigi <williamdiluigi@gmail.com>
2025-03-15 20:12:33 +0100

    Add logger when running in development mode.

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,9 @@
 import { drizzle as drizzleSqlite } from "drizzle-orm/libsql";
 import { drizzle as drizzlePostgres } from "drizzle-orm/node-postgres";
 
-export const cmsDb = drizzlePostgres(process.env.CMS_DATABASE_URL!);
-export const terryDb = drizzleSqlite(process.env.TERRY_DATABASE_URL!);
+export const cmsDb = drizzlePostgres(process.env.CMS_DATABASE_URL!, {
+    logger: process.env.NODE_ENV === 'development',
+});
+export const terryDb = drizzleSqlite(process.env.TERRY_DATABASE_URL!, {
+    logger: process.env.NODE_ENV === 'development',
+});
