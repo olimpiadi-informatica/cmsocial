a2f8aaa0325ba9d0fee3f91df806b6e1ae6bd446
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2025-03-16 13:51:19 +0100

    Validate jwt

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,9 +1,11 @@
 import { cookies } from "next/headers";
 
-import { getMeSync } from "@olinfo/training-api";
+import { getMe, getMeSync } from "@olinfo/training-api";
 
 export async function getSessionUser() {
   const token = (await cookies()).get("training_token")?.value;
   if (!token) return null;
-  return getMeSync(token);
+  const me = getMeSync(token);
+  if (me.username !== (await getMe())?.username) return null;
+  return me;
 }
