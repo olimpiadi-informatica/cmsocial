0556d14441d0837d576aad35573b1f89dce7a3ce
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2025-06-27 21:44:47 +0200

    Fix head

--- src/lib/emails/template.tsx
+++ src/lib/emails/template.tsx
@@ -1,14 +1,13 @@
-import Head from "next/head";
 import type { ReactNode } from "react";
 
 export function Template({ title, children }: { title: string; children: ReactNode }) {
   return (
     <html lang="it">
-      <Head>
+      <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-      </Head>
+      </head>
       <body style={{ fontFamily: "sans-serif", fontSize: "16px", padding: "16px" }}>
         <div style={{ fontSize: "1.75em", fontWeight: "bold", textAlign: "center" }}>
           Olimpiadi di Informatica
